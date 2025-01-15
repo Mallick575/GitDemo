3c9efcba7916df0bb99779919f852bc1ca659583
Mallick575 <maniruddinmallick575@gmail.com>
2025-01-15 16:44:49 +0530

    American development team

--- GitProject/src/code/ProgramRun.java
+++ GitProject/src/code/ProgramRun.java
@@ -25,4 +25,26 @@ public class ProgramRun {
 
 	}
 
+	public static void imp1() 
+	{
+	
+		System.out.println("I am Maniruddin Mallick");
+		System.out.println("i am change");
+		System.out.println("i am change");
+		System.out.println("i am change");
+		
+		System.out.println("Maniruddin Mallick");
+
+	}
+	public static void imp2() 
+	{
+	
+		System.out.println("I am Maniruddin Mallick");
+		System.out.println("i am change");
+		System.out.println("i am change");
+		System.out.println("i am change");
+		
+		System.out.println("Maniruddin Mallick");
+
+	}
 }
